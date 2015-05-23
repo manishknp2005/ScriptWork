beee84e18abc009c690c7e5493c3da67caeed1a2
manishknp2005 <manishknp2005@gmail.com>
2015-05-23 23:47:13 +0530

    Update PasswordValidator.sh

--- PasswordValidator.sh
+++ PasswordValidator.sh
@@ -3,7 +3,10 @@
 echo "Please enter a valid passowrd"
 read pass
 
-regex="/^[A-Za-z0-9#$+*]{8,}$/"
+regex="/^[A-Za-z0-9#$+*]{8,}$/"    //This creates a character class consisting of the legal characters, 
+                                   //and quantifies how many such characters are needed; a minimum of eight. 
+                                   //It anchors to the start and the end of the string, so that the string must 
+                                   //contain only those characters, from start to finish.
 
 if [ $pass =~ $regex ]
 then
